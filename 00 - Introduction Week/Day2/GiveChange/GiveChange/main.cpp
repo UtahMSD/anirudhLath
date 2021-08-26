86c2a6d4a42a77741127f1fc65beb3d3de71d176
Anirudh Lath <anirudhlath@gmail.com>
2021-08-25 21:29:06 -0600

    Updated GiveChange for Day3 requirements.

--- 00 - Introduction Week/Day2/GiveChange/GiveChange/main.cpp
+++ 00 - Introduction Week/Day2/GiveChange/GiveChange/main.cpp
@@ -16,10 +16,30 @@ int main(int argc, const char * argv[]) {
     // Enter the Item Price
     std::cout << "Please enter the item price below in cents:\n";
     std::cin >> itemPrice;
-    
+
+    if (itemPrice < 0) {
+        std::cout << "\nError: The item price cannot be negative. The program will exit.\n";
+        return 1;
+    }
+
     // Enter Amount Paid
-    std::cout << "Please enter the amount paid below in cents:\n";
+    std::cout << "\nPlease enter the amount paid below in cents:\n";
     std::cin >> amountPaid;
+
+    // Error Handling
+    if (amountPaid < 0) {
+        std::cout << "\nError: The amount paid cannot be negative. The program will exit.\n";
+        return 1;
+    }
+    else if (amountPaid < itemPrice) {
+        std::cout << "\nInsufficient Funds! Please try again!\n";
+    }
+
+    // Asking the user for input until he pays the amount required.
+    while (amountPaid < itemPrice) {
+        std::cout << "\nPlease enter the amount paid below in cents:\n";
+        std::cin >> amountPaid;
+    }
     
     // Logic
     int change = amountPaid - itemPrice;
@@ -30,13 +50,53 @@ int main(int argc, const char * argv[]) {
     int nickels = centsLeftInDimes / 5;
     int centsLeftInNickels = centsLeftInDimes % 5;
     int pennies = centsLeftInNickels;
+
+    if (quarters > 2) {
+        quarters = 2;
+        centsLeftInQuarters = (change - (quarters * 25));
+        dimes = centsLeftInQuarters / 10;
+        if (dimes > 2) {
+            dimes = 2;
+            centsLeftInDimes = (change - (dimes * 10));
+            nickels = centsLeftInDimes / 5;
+            if (nickels > 2) {
+                nickels = 2;
+                centsLeftInNickels = (change - (nickels * 5));
+                pennies = centsLeftInNickels;
+                if (pennies > 2) {
+                    std::cout << "There is not enough change to return, therefore we cannot sell you the product and here is your " << amountPaid << " cents back :)\n";
+                }
+            }
+            else {
+                nickels = centsLeftInDimes / 5;
+                centsLeftInNickels = centsLeftInDimes % 5;
+                pennies = centsLeftInNickels;
+            }
+        }
+        else {
+            dimes = centsLeftInQuarters / 10;
+            centsLeftInDimes = centsLeftInQuarters % 10;
+            nickels = centsLeftInDimes / 5;
+            centsLeftInNickels = centsLeftInDimes % 5;
+            pennies = centsLeftInNickels;
+        }
+    }
+    else {
+        quarters = change / 25;
+        centsLeftInQuarters = change % 25;
+        dimes = centsLeftInQuarters / 10;
+        centsLeftInDimes = centsLeftInQuarters % 10;
+        nickels = centsLeftInDimes / 5;
+        centsLeftInNickels = centsLeftInDimes % 5;
+        pennies = centsLeftInNickels;
+    }
     
     // Output the change
-    std::cout << "Change = " << change << " cents\n";
+    std::cout << "\nChange = " << change << " cents\n";
     std::cout << "Quarters: " << quarters << std::endl;
     std::cout << "Dimes: " << dimes << std::endl;
     std::cout << "Nickels: " << nickels << std::endl;
-    std::cout << "Pennies: " << pennies << std::endl;
+    std::cout << "Pennies: " << pennies << std::endl << "\n";
     
     return 0;
 }
