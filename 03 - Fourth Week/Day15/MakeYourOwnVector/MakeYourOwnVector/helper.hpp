958ac3f93a54949e0effc84a0aa9d79a7829105a
Anirudh Lath <anirudhlath@gmail.com>
2021-09-18 20:24:44 -0600

    Moved all the code from helper.cpp to helper.hpp due to template constraints. Improved tests. Added operation overloaded. Fixed a few bugs. Demonstrated that it works with int, double and char. Probably works with more types just haven't test every type.

--- 03 - Fourth Week/Day15/MakeYourOwnVector/MakeYourOwnVector/helper.hpp
+++ 03 - Fourth Week/Day15/MakeYourOwnVector/MakeYourOwnVector/helper.hpp
@@ -9,12 +9,15 @@
 #define helper_hpp
 
 #include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
 
+template <typename T>
 class myvector {
 private:
     
     // Variables
-    int* index;
+    T * index;
     int capacity;
     int size;
     
@@ -23,29 +26,258 @@ public:
     // Constructor
     myvector();
     myvector( int initialCapacity );
-    myvector(const myvector & vec);
+    myvector(const myvector<T> & vec);
     
     
     
     // Public Methods
     void growVector();
-    int getSize() const;
-    int* getAddress() const;
-    int getCapacity() const;
-    int getIndex(int i) const;
-    void pushBack(int num);
+    int getSize() const {return size;}
+    T* getAddress() const {return index;}
+    int getCapacity() const {return capacity;}
+    //int getIndex(int i) const {return index[i];}
+    void pushBack(T data);
     void popBack();
-    int get(int i) const;
-    void set(int i, int newValue);
+    T get(int i) const;
+    void set(int i, T newValue);
     
     // Destructor
     ~myvector();
     
     // Operation overload
     myvector& operator=(const myvector & rhs);
-    int operator[](int i) const;
-    int & operator[](int i);
+    T operator[](int i) const;
+    T & operator[](int i);
+    bool operator==(myvector<T> rhs);
+    bool operator!=(myvector<T> rhs);
+    bool operator>(myvector<T> rhs);
+    bool operator>=(myvector<T> rhs);
+    bool operator<=(myvector<T> rhs);
+    bool operator<(myvector<T> rhs);
     
 };
+
+template <typename T>
+myvector<T>::myvector(){
+    capacity = 10;
+    size = 0;
+    index = new T[10];
+} // Default Constructor
+
+template <typename T>
+myvector<T>::myvector( int initialCapacity ) {
+    capacity = initialCapacity;
+    size = 0;
+    index = new T[initialCapacity];
+} // Constructor with initial capacity param
+
+template <typename T>
+myvector<T>::myvector(const myvector & rhs) {
+    T * newData = new T[rhs.capacity]; // Create own memory allocation
+    for(int i = 0; i < rhs.size; i++) {
+        newData[i] = rhs.index[i];
+    }
+    capacity = rhs.capacity;
+    size = rhs.size;
+    index = newData;
+    
+    if(index == rhs.index) {
+        std::cout << "Memory reallocation warning. Please check the code." << std::endl;
+        exit(1); // If this and rhs are pointing to the same address in memory, notify the user about it. Simple error check.
+    }
+} // Copy Constructor
+
+template <typename T>
+myvector<T>::~myvector() {
+    index = nullptr;
+    delete [] index;
+    capacity = 0;
+} // Destructor
+
+template <typename T>
+void myvector<T>::pushBack(T data) {
+    if (size < capacity) {
+        index[size] = data;
+    } else {
+        growVector();
+        index[size] = data;
+    }
+    size++;
+}
+
+template <typename T>
+void myvector<T>::popBack() {
+    if (size > 0) {
+        size--;
+        index[size] = NULL;
+    } else {
+        std::cout << "There are no elements in this vector to pop." << std::endl;
+        exit(1);
+    }
+    
+
+}
+
+template <typename T>
+T myvector<T>::get(int i) const {
+    if (i >= size) {
+        std::cout << "The index you have entered is out of range." << std::endl;
+        exit(1);
+    }
+    return index[i];
+}
+
+template <typename T>
+void myvector<T>::set(int i, T newValue) {
+    if (i < size) {
+        *(index + i) = newValue;
+    } else {
+        std::cout << "The index you have entered is out of range. Please try again with a valid index." << std::endl;
+        exit(1);
+    }
+}
+
+template <typename T>
+void myvector<T>::growVector() {
+    capacity *= 2;
+    myvector temp(*this);
+    this -> ~myvector();
+    index = temp.index;
+    size = temp.size;
+    capacity = temp.capacity;
+}
+
+template <typename T>
+myvector<T>& myvector<T>::operator=(const myvector & rhs) {
+    // Reassignment memory leak protection
+    if (this == &rhs) {
+        return *this;
+    }
+    
+    T * newData = new T[rhs.capacity];
+    for(int i = 0; i < rhs.size; i++) {
+        newData[i] = rhs.index[i];
+    }
+    capacity = rhs.capacity;
+    size = rhs.size;
+    delete [] index;
+    index = newData;
+    
+    if(index == rhs.index) {
+        std::cout << "Memory reallocation warning. Please check the code." << std::endl;
+        exit(1); // If this and rhs are pointing to the same address in memory, notify the user about it. Simple error check.
+    }
+    
+    return *this;
+}
+
+template <typename T>
+T myvector<T>::operator[](int i) const {
+    if (i < 0 or i >= size) {
+        std::cout << "The index you have entered is out of range. Program will not exit." << std::endl;
+        exit(1);
+    }
+    int value = index[i];
+    return value;
+}
+
+template <typename T>
+T & myvector<T>::operator[](int i) {
+    if (i < 0 or i >= size) {
+        std::cout << "The index you have entered is out of range. Program will not exit." << std::endl;
+        exit(1);
+    }
+    return index[i];
+}
+
+template <typename T>
+bool myvector<T>::operator==(myvector<T> rhs) {
+    if (size == rhs.size) {
+        for(int i = 0; i < size; i++) {
+            if (index[i] != rhs.index[i]) {
+                return false;
+            }
+        }
+    } else {
+        std::cout << "The objects cannot be compared they are of different sizes.";
+        exit(1);
+    }
+    return true;
+}
+
+template <typename T>
+bool myvector<T>::operator!=(myvector<T> rhs) {
+    if (size == rhs.size) {
+        for(int i = 0; i < size; i++) {
+            if (index[i] == rhs.index[i]) {
+                return false;
+            }
+        }
+    } else {
+        std::cout << "The objects cannot be compared they are of different sizes.";
+        exit(1);
+    }
+    return true;
+}
+
+template <typename T>
+bool myvector<T>::operator>(myvector<T> rhs) {
+    if (size == rhs.size) {
+        for(int i = 0; i < size; i++) {
+            if (index[i] <= rhs.index[i]) {
+                return false;
+            }
+        }
+    } else {
+        std::cout << "The objects cannot be compared they are of different sizes.";
+        exit(1);
+    }
+    return true;
+}
+
+template <typename T>
+bool myvector<T>::operator>=(myvector<T> rhs) {
+    if (size == rhs.size) {
+        for(int i = 0; i < size; i++) {
+            if (index[i] < rhs.index[i]) {
+                return false;
+            }
+        }
+    } else {
+        std::cout << "The objects cannot be compared they are of different sizes.";
+        exit(1);
+    }
+    return true;
+}
+
+template <typename T>
+bool myvector<T>::operator<=(myvector<T> rhs) {
+    if (size == rhs.size) {
+        for(int i = 0; i < size; i++) {
+            if (index[i] > rhs.index[i]) {
+                return false;
+            }
+        }
+    } else {
+        std::cout << "The objects cannot be compared they are of different sizes.";
+        exit(1);
+    }
+    return true;
+}
+
+template <typename T>
+bool myvector<T>::operator<(myvector<T> rhs) {
+    if (size == rhs.size) {
+        for(int i = 0; i < size; i++) {
+            if (index[i] >= rhs.index[i]) {
+                return false;
+            }
+        }
+    } else {
+        std::cout << "The objects cannot be compared they are of different sizes.";
+        exit(1);
+    }
+    return true;
+}
 
 #endif /* helper_hpp */
--- 03 - Fourth Week/Day15/MakeYourOwnVector/MakeYourOwnVector/main.cpp
+++ 03 - Fourth Week/Day15/MakeYourOwnVector/MakeYourOwnVector/main.cpp
@@ -9,103 +9,113 @@
 
 
 
-void makeVectorTest(const myvector &testVector, const myvector &testVector2, const myvector &testVector3) {
+template <typename T>
+void makeVectorTest(const myvector<T> &testVector, const myvector<T> &testVector2, const myvector<T> &testVector3) {
     if (testVector.getCapacity() == 4 && testVector.getSize() == 0
-        && testVector2.getCapacity() == 10 && testVector2.getSize() == 0
-        && testVector3.getCapacity() == 129 && testVector3.getSize() == 0)
+    && testVector2.getCapacity() == 10 && testVector2.getSize() == 0
+    && testVector3.getCapacity() == 129 && testVector3.getSize() == 0) // Check the class parameters to see if it is the expected value.
     {
         std::cout << "T1: makeVector() passed!\n";
     } else {
         std::cout << "T1: makeVector() failed!!!\n";
         exit(1);
     }
+
+    // Print Stats
     std::cout << " Statistics of each vector:\n";
     std::cout << " testVector  ===> Size = " << testVector.getSize() << " | Capacity:   " << testVector.getCapacity() << " | Address: " << testVector.getAddress() << "\n";
     std::cout << " testVector2 ===> Size = " << testVector2.getSize() << " | Capacity:  " << testVector2.getCapacity() << " | Address: " << testVector2.getAddress() << "\n";
     std::cout << " testVector3 ===> Size = " << testVector3.getSize() << " | Capacity: " << testVector3.getCapacity() << " | Address: " << testVector3.getAddress() << "\n\n";
-}
+} // Make Vector Test
 
-static void pushBackTest(myvector &testVector, myvector &testVector2, myvector &testVector3) {
-    for(int i = 0; i < 150; i++) {
+template <typename T>
+static void pushBackTest(myvector<T> &testVector, myvector<T> &testVector2, myvector<T> &testVector3) {
+    for(int i = 0; i < 150; i++) { // Push 150 elements into each vector.
         testVector.pushBack(i);
         testVector2.pushBack(i);
         testVector3.pushBack(i);
-        if (testVector.getSize() != i + 1 && testVector.getIndex(i) != i
-            && testVector2.getSize() != i + 1 && testVector2.getIndex(i) != i
-            && testVector3.getSize() != i + 1 && testVector3.getIndex(i) != i) {
+        if (testVector.getSize() != i + 1 && testVector[i] != i
+        && testVector2.getSize() != i + 1 && testVector2[i] != i
+        && testVector3.getSize() != i + 1 && testVector3[i] != i) { // Check the size and the value that was copied.
             std::cout << "T2: pushBack() failed!!!\n";
             exit(1);
         }
     }
-    
+    // Print Stats
     std::cout << "T2: pushBack() passed!\n";
     std::cout << " Statistics of each vector:\n";
     std::cout << " *** NOTICE THE ADDRESS CHANGED DUE TO growVector() ***\n";
     std::cout << " testVector  ===> Size = " << testVector.getSize() << " | Capacity: " << testVector.getCapacity() << " | Address: " << testVector.getAddress() << "\n";
     std::cout << " testVector2 ===> Size = " << testVector2.getSize() << " | Capacity: " << testVector2.getCapacity() << " | Address: " << testVector2.getAddress() << "\n";
     std::cout << " testVector3 ===> Size = " << testVector3.getSize() << " | Capacity: " << testVector3.getCapacity() << " | Address: " << testVector3.getAddress() << "\n\n";
-}
+} // pushBack() Test
 
-static void CopyConstructorTest(myvector &testVector) {
-    myvector testCopy(testVector);
-    
-    for(int i = 0; i < 150; i++) {
+template <typename T>
+static void CopyConstructorTest(myvector<T> &testVector) {
+    myvector<int> testCopy(testVector); // Use copy constructor.
+
+    for(int i = 0; i < 150; i++) { // Compare every element in the array and if it doesn't match exit program and fail the test.
         if(testCopy[i] != testVector[i] or testCopy.getSize() != testVector.getSize() or testCopy.getCapacity() != testVector.getCapacity() or testCopy.getAddress() == testVector.getAddress()) {
             std::cout << "T9: Copy Constructor failed!!!\n\n";
             exit(1);
         }
     }
-    
+    // Print Stats
     std::cout << "T9: Copy Constructor passed!!!\n";
     std::cout << " Statistics of each vector:\n";
     std::cout << " *** NOTICE THE ADDRESS CHANGED ***\n";
     std::cout << " testVector  ===> Size = " << testVector.getSize() << " | Capacity: " << testVector.getCapacity() << " | Address: " << testVector.getAddress() << "\n";
     std::cout << " testCopy    ===> Size = " << testCopy.getSize() << " | Capacity: " << testCopy.getCapacity() << " | Address: " << testCopy.getAddress() << "\n\n";
-}
+} // Copy Constructor Test
 
-static void OperationOverloadTest(myvector &testVector) {
-    myvector testOperator;
-    testOperator = testVector;
-    int beforeValue = testVector[10];
-    
-    testVector[10] = 1000;
-    //std::cout << "Testing operator testVector[10] = 1000 ==> " << testVector[10] << std::endl;
-    if (testVector[10] != 1000) {
+template <typename T>
+static void OperationOverloadTest(myvector<T> &testVector) {
+    myvector<int> testOperator;
+    testOperator = testVector; // Use '=' operator to test
+    int beforeValue = testVector[10]; // Record the value at index 10 before change.
+
+    testVector[10] = 1000; // Assign the 10th value to 1000.
+    if (testVector[10] != 1000) { // Check if the value changed to 1000 or not.
         std::cout << "T10: Operation Overload '[]' failed!!!\n";
         exit(1);
     }
+    // Print Stats
     std::cout << "T10: Operation Overload '[]' passed!\n";
     std::cout << "testVector[10] before assignment was: " << beforeValue << std::endl;
     std::cout << "testVector[10] before assignment is:  " << testVector[10] << "\n";
     std::cout << "testOperator[10] value after assign:  " <<testOperator[10] << "\n";
-    testVector[10] = 10;
+    testVector[10] = 10; // Reassign the value to 10 so that it doesn't break the other tests.
     std::cout << " *** TestVector[10] reverted to a value of " << beforeValue << " ***\n\n";
-    
-    for(int i = 0; i < 150; i++) {
+
+    // Test for '='
+    for(int i = 0; i < 150; i++) { // Check if every member variable and data in array matches or not.
         if(testOperator[i] != testVector[i] or testOperator.getSize() != testVector.getSize() or testOperator.getCapacity() != testVector.getCapacity() or testOperator.getAddress() == testVector.getAddress()) {
             std::cout << "T11: Operation Overload '=' failed!!!\n";
             exit(1);
         }
     }
     std::cout << "T11: Operation Overload '=' passed!\n";
-    
+
+    // Print Stats
     std::cout << " Statistics of each vector:\n";
     std::cout << " *** NOTICE THE ADDRESS CHANGED ***\n";
     std::cout << " testVector   ===> Size = " << testVector.getSize() << " | Capacity: " << testVector.getCapacity() << " | Address: " << testVector.getAddress() << "\n";
     std::cout << " testOperator ===> Size = " << testOperator.getSize() << " | Capacity: " << testOperator.getCapacity() << " | Address: " << testOperator.getAddress() << "\n\n";
-    
-}
 
-static void getTest(const myvector &testVector, const myvector &testVector2, const myvector &testVector3) {
-    for (int i = 0; i < 150; i++) {
+} // Operation Overload Test
+
+template <typename T>
+static void getTest(const myvector<T> &testVector, const myvector<T> &testVector2, const myvector<T> &testVector3) {
+    for (int i = 0; i < 150; i++) { // get every element and compare it with the expected value.
         if (testVector.get(i) != i
-            && testVector2.get(i) != i
-            && testVector3.get(i) != i ) {
+        && testVector2.get(i) != i
+        && testVector3.get(i) != i ) {
             std::cout << "T3: get() failed!!!\n";
             exit(1);
         }
-    }
-    
+    } // get() Test
+
+    // Print Stats
     std::cout << "T3: get() passed!\n";
     std::cout << " Statistics of each vector:\n";
     std::cout << " testVector  ===> Size = " << testVector.getSize() << " | Capacity: " << testVector.getCapacity() << " | Address: " << testVector.getAddress() << "\n";
@@ -113,101 +123,184 @@ static void getTest(const myvector &testVector, const myvector &testVector2, con
     std::cout << " testVector3 ===> Size = " << testVector3.getSize() << " | Capacity: " << testVector3.getCapacity() << " | Address: " << testVector3.getAddress() << "\n\n";
 }
 
-static void setTest(myvector &testVector, myvector &testVector2, myvector &testVector3) {
-    for (int i = 150; i > 0; i--) {
+template <typename T>
+static void setTest(myvector<T> &testVector, myvector<T> &testVector2, myvector<T> &testVector3) {
+    for (int i = 150; i > 0; i--) { // set a new value to every element in all the given myvector objects. I am reversing the data using set here.
         testVector.set(i - 1, 150 - i);
         testVector2.set(i - 1, 150 - i);
         testVector3.set(i - 1, 150 - i);
         if (   testVector.get(i - 1) != 150 - i
-            && testVector2.get(i - 1) != 150 - i
-            && testVector3.get(i - 1) != 150 - i) {
+        && testVector2.get(i - 1) != 150 - i
+        && testVector3.get(i - 1) != 150 - i) { // If the data doesn't change fail the test.
             std::cout << "T4: set() failed!!!\n";
             exit(1);
         }
     }
+    // Print Stats
     std::cout << "T4: set() passed!\n";
     std::cout << " Statistics of each vector:\n";
     std::cout << " testVector  ===> Size = " << testVector.getSize() << " | Capacity: " << testVector.getCapacity() << " | Address: " << testVector.getAddress() << "\n";
     std::cout << " testVector2 ===> Size = " << testVector2.getSize() << " | Capacity: " << testVector2.getCapacity() << " | Address: " << testVector2.getAddress() << "\n";
     std::cout << " testVector3 ===> Size = " << testVector3.getSize() << " | Capacity: " << testVector3.getCapacity() << " | Address: " << testVector3.getAddress() << "\n\n";
-}
+} // set() Test
 
-static void growVectorTest(myvector &testVector, myvector &testVector2, myvector &testVector3) {
+template <typename T>
+static void growVectorTest(myvector<T> &testVector, myvector<T> &testVector2, myvector<T> &testVector3) {
+    // Record the initial capacity of the vectors.
     int vec1 = testVector.getCapacity();
     int vec2 = testVector2.getCapacity();
     int vec3 = testVector3.getCapacity();
+
+    // Grow all the vectors.
     testVector.growVector();
     testVector2.growVector();
     testVector3.growVector();
-    
+
     if (   testVector.getCapacity() != vec1 * 2
-        && testVector2.getCapacity() != vec2 * 2
-        && testVector3.getCapacity() != vec3 * 2) {
+    && testVector2.getCapacity() != vec2 * 2
+    && testVector3.getCapacity() != vec3 * 2) { // If the capacity of each vector hasn't doubled, fail the test.
         std::cout << "T5: growVector() failed!!!\n";
         exit(1);
     }
+    // Print Stats
     std::cout << "T5: growVector() passed!\n";
     std::cout << " Statistics of each vector:\n";
     std::cout << " testVector  ===> Size = " << testVector.getSize() << " | Capacity: " << testVector.getCapacity() << " | Address: " << testVector.getAddress() << "\n";
     std::cout << " testVector2 ===> Size = " << testVector2.getSize() << " | Capacity: " << testVector2.getCapacity() << " | Address: " << testVector2.getAddress() << "\n";
     std::cout << " testVector3 ===> Size = " << testVector3.getSize() << " | Capacity: " << testVector3.getCapacity() << " | Address: " << testVector3.getAddress() << "\n\n";
-}
+} // growVector() Test
 
-static void pushBackTests(myvector &testVector, myvector &testVector2, myvector &testVector3) {
-    for(int i = 0; i < 150; i++) {
+template <typename T>
+static void popBackTest(myvector<T> &testVector, myvector<T> &testVector2, myvector<T> &testVector3) {
+    for(int i = 0; i < 150; i++) { // pop each element in each vector.
         testVector.popBack();
         testVector2.popBack();
         testVector3.popBack();
-        if (testVector.getSize() != 150 - i - 1 && testVector.getIndex(i) != 0
-            && testVector2.getSize() != 150 - i - 1 && testVector2.getIndex(i) != 0
-            && testVector3.getSize() != 150 - i - 1 && testVector3.getIndex(i) != 0) {
+        if (testVector.getSize() != 150 - i - 1 && testVector[i] != 0
+        && testVector2.getSize() != 150 - i - 1 && testVector2[i] != 0
+        && testVector3.getSize() != 150 - i - 1 && testVector3[i] != 0) { // If the size hasn't decreased and the old value hasn't changed to null fail the test.
             std::cout << "T6: popBack() failed!!!\n";
             exit(1);
         }
     }
+    // Print Test
     std::cout << "T6: popBack() passed!\n";
     std::cout << " Statistics of each; vector:\n";
     std::cout << " testVector  ===> Size = " << testVector.getSize() << " | Capacity: " << testVector.getCapacity() << " | Address: " << testVector.getAddress() << "\n";
     std::cout << " testVector2 ===> Size = " << testVector2.getSize() << " | Capacity: " << testVector2.getCapacity() << " | Address: " << testVector2.getAddress() << "\n";
     std::cout << " testVector3 ===> Size = " << testVector3.getSize() << " | Capacity: " << testVector3.getCapacity() << " | Address: " << testVector3.getAddress() << "\n\n";
-}
+} // popBack() Test
 
-static void DestructorTest(myvector &testVector, myvector &testVector2, myvector &testVector3) {
+template <typename T>
+static void DestructorTest(myvector<T> &testVector, myvector<T> &testVector2, myvector<T> &testVector3) {
+    // Record the current memory address of each vector.
     int *myvec1 = testVector.getAddress();
     int *myvec2 = testVector2.getAddress();
     int *myvec3 = testVector3.getAddress();
-    
-    myvector vec4(testVector);
-    
+
+    // Destroy all the vectors
     testVector.~myvector();
     testVector2.~myvector();
     testVector3.~myvector();
-    
+
     if (   testVector.getAddress() == myvec1
-        && testVector2.getAddress() == myvec2
-        && testVector3.getAddress() == myvec3) {
+    && testVector2.getAddress() == myvec2
+    && testVector3.getAddress() == myvec3) { // If the destroyed vectors still point to the same memory addresses fail the test.
         std::cout << "T7: ~myvector() failed!!!\n";
         exit(1);
     }
     std::cout << "T7: ~myvector() passed!\n";
-    
+
     std::cout << " Statistics of each vector:\n";
     std::cout << " testVector  ===> Size = " << testVector.getSize() << " | Capacity: " << testVector.getCapacity() << " | Address: " << testVector.getAddress() << "\n";
     std::cout << " testVector2 ===> Size = " << testVector2.getSize() << " | Capacity: " << testVector2.getCapacity() << " | Address: " << testVector2.getAddress() << "\n";
     std::cout << " testVector3 ===> Size = " << testVector3.getSize() << " | Capacity: " << testVector3.getCapacity() << " | Address: " << testVector3.getAddress() << "\n\n";
-}
+} // Destructor Test
 
 
+
+static void templateTest(myvector<double> &doubleVector, myvector<int> &intVector, myvector<char> &strVector) {
+    // Append values
+    for (int i = 0 ; i < 4; i++) {
+        intVector.pushBack(i);
+        doubleVector.pushBack(1.01 * i);
+        strVector.pushBack('A' + i);
+    }
+
+    // Test
+    for (int i = 0; i < 4; i++) {
+        if (intVector[i] != i or doubleVector[i] != 1.01 * i or strVector[i] != 'A' + i) {
+            std::cout << "T12: Templates failed!!!\n";
+            exit(1);
+        }
+    }
+    std::cout << "T12: Templates passed!\n";
+
+    // Print out the vectors
+    std::cout << "intVector: { ";
+    for (int i = 0;  i < 4; i++) {
+        if (i < 3) {
+            std::cout << intVector[i] << ", ";
+        } else {
+            std::cout << intVector[i];
+        }
+
+    }
+    std::cout << " }\n";
+
+    std::cout << "doubleVector: { ";
+    for (int i = 0;  i < 4; i++) {
+        if(i < 3) {
+            std::cout << doubleVector[i] << ", ";
+        } else {
+            std::cout << doubleVector[i];
+        }
+
+    }
+    std::cout << " }\n";
+
+    std::cout << "strVector: { ";
+    for (int i = 0;  i < 4; i++) {
+        if (i < 3) {
+            std::cout << strVector[i] << ", ";
+        } else {
+            std::cout << strVector[i];
+        }
+    }
+    std::cout << " }\n\n";
+
+} // Template Test
+
+static void booleanOperatorOverloadTest(myvector<int> &intVector) {
+    myvector<int> intCopy(intVector);
+
+    if(intVector != intCopy or
+    intVector > intCopy or
+    intVector < intCopy) { // Compare and see if any values are not the same, fail the test.
+        std::cout << "T13: Boolean operator overload failed!!!\n";
+        exit(1);
+    }
+    if (intVector == intCopy or
+    intVector >= intCopy or
+    intVector <= intCopy) { // Compare and see if all the values are the same, pass the test.
+        std::cout << "T13: Boolean operator overload passed!\n\n";
+    }
+} // Boolean operator overload test.
 
 int main(int argc, const char * argv[]) {
-    
-    myvector testVector(4);
-    myvector testVector2(10);
-    myvector testVector3(129);
-    
+
+    myvector<int> testVector(4);
+    myvector<int> testVector2(10);
+    myvector<int> testVector3(129);
+    myvector<int> intVector(4);
+    myvector<double> doubleVector(4);
+    myvector<char> strVector(4);
+
+
+
     // T1: Make Vector Test
     makeVectorTest(testVector, testVector2, testVector3);
-    
+
     // T2: pushBack Test
     pushBackTest(testVector, testVector2, testVector3);
 
@@ -216,27 +309,32 @@ int main(int argc, const char * argv[]) {
 
     // T10, T11: Operation Overload Test
     OperationOverloadTest(testVector);
-   
+
     // T3: get Test
     getTest(testVector, testVector2, testVector3);
-    
+
     // T4: set Test
     setTest(testVector, testVector2, testVector3);
-    
+
     // T5: growVector Test
     growVectorTest(testVector, testVector2, testVector3);
 
     // T6: pushBack Test
-    pushBackTests(testVector, testVector2, testVector3);
-    
+    popBackTest(testVector, testVector2, testVector3);
+
     // T7: Destructor Test
     DestructorTest(testVector, testVector2, testVector3);
-    
-    
-    
+
+    // T12: Template Test
+    templateTest(doubleVector, intVector, strVector);
+
+    // T13: Boolean Operator Overload Test
+    booleanOperatorOverloadTest(intVector);
+
+
     std::cout << "[******************** ALL TESTS PASSED! ********************]\n\n";
-    
-    
-    
+
+
+
     return 0;
 }
