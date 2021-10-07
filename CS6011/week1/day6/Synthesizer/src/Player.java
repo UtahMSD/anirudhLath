a512d656df316e1b8e4184f1507d04836bf800d9
Anirudh Lath <anirudhlath@gmail.com>
2021-10-07 11:26:01 -0600

    Cleaned up the extra commented out code.

--- CS6011/week1/day6/Synthesizer/src/Player.java
+++ CS6011/week1/day6/Synthesizer/src/Player.java
@@ -8,7 +8,6 @@ public class Player {
     private static AudioComponent mixer;
     public static AudioClip clip;
     public static AudioFormat format;
-    public static int soundCount_ = 0;
 
     private AudioFormat format16 = new AudioFormat(44100, 16, 1, true, false);
     private AudioFormat format24 = new AudioFormat(44100, 24, 1, true, false);
@@ -39,21 +38,15 @@ public class Player {
     }
 
     public static void play(int loop) throws LineUnavailableException {
-        //update();
         c.open(format, clip.getData(), 0, clip.getData().length);
         System.out.println("Playing...");
         c.start();
         c.loop(loop);
-//        while (c.getFramePosition() < AudioClip.TOTAL_SAMPLES || c.isActive() || c.isRunning()) {
-//        }
-//        System.out.println("Done playing " + (loop + 1) + " times.");
-//        c.close();
     }
 
     public static void replay(int loop) throws LineUnavailableException {
         c.stop();
         c.close();
-        //update();
         play(loop);
     }
 }
--- CS6011/week1/day6/Synthesizer/src/VFSineWave.java
+++ CS6011/week1/day6/Synthesizer/src/VFSineWave.java
@@ -6,17 +6,9 @@ public class VFSineWave implements AudioComponent{
     public AudioClip getClip() {
         AudioClip clip = new AudioClip();
         double phase = 0.0;
-        for (int i = 0; i < clip.samplesArray.length; i++) { // Configured for a square wave of int frequency, the
-            // highest possible amplitude.
+        for (int i = 0; i < clip.samplesArray.length; i++) {
             phase += (2 * Math.PI * inputClip.getSample(i)) / inputClip.samplesArray.length;
             clip.setSample(i, (short) (Short.MAX_VALUE * Math.sin(phase)));
-//            if (((float) (frequency * i * 2 / AudioClip.sampleRate)) % 2 > 0.5) { // ((float)(frequency * i * 2 /
-//                // AudioClip.sampleRate)) % 2 was always equal to zero, therefore I used 2 to make the algorithm work
-//                // and multiplied 2 to the frequency to compensate for the %2.
-//                clip.setSample(i, Short.MAX_VALUE);
-//            } else {
-//                clip.setSample(i, Short.MIN_VALUE);
-//            }
         }
         return clip;
     }
