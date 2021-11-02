052a8b7686b1b84c51a95db564349f4c06e7d496
Anirudh Lath <anirudhlath@gmail.com>
2021-11-01 22:42:07 -0600

    Added a few things.

--- CS6011/week5/day21/AndroidChatClient/app/src/main/java/com/anirudhlath/androidchatclient/ChatActivity.java
+++ CS6011/week5/day21/AndroidChatClient/app/src/main/java/com/anirudhlath/androidchatclient/ChatActivity.java
@@ -3,6 +3,12 @@ package com.anirudhlath.androidchatclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -11,6 +17,16 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTitle(MainActivity.roomName);
         setContentView(R.layout.activity_chat);
+    }
+
+    public void sendbutton(View view) {
+        EditText chatMessageField = findViewById(R.id.chatMessageTE);
+        String currentMessage = chatMessageField.getText().toString();
+        ListView chatView = findViewById(R.id.chatViewLV);
+        String currentUser = MainActivity.userName;
+        String composedElement = currentUser + ": " + currentMessage;
+        ArrayList<String> messagesArray = new ArrayList<>();
+//        ArrayAdapter<String> messagesArrayAdapter = new ArrayAdapter<String>(this, chatView);
     }
 
 }
