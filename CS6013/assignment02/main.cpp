ae26b7396d94a4106866d96ed340d171c4990eb5
Anirudh Lath <u1149016@lab1-11.eng.utah.edu>
2022-02-03 20:01:47 -0700

    Updated asm

--- CS6013/assignment02/main.cpp
+++ CS6013/assignment02/main.cpp
@@ -12,9 +12,14 @@ int main() {
 	const char *s;
 	int len = 3;
 
+	std::cout << "_sayHello():\n";
 	_sayHello();
+
+	std::cout << "\nmyPuts():\n";
 	myPuts(s, len);
+
+	std::cout << "\nmyGTOD():\n";
 	std::cout << myGTOD().tv_sec;
-	std::cout << myGTOD().tv_usec;
+	std::cout << myGTOD().tv_usec << std::endl;
 	return 0;
 }
