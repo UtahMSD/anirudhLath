5c3e5b68563129adcef015fe1577ccf100bb27e4
anirudhlath <anirudhlath@gmail.com>
2022-05-03 18:27:31 -0600

    fixed bugs

--- CS6013/assignment06/main.cpp
+++ CS6013/assignment06/main.cpp
@@ -37,13 +37,15 @@ auto parallel_sum_std(T a[], size_t N, size_t num_threads) {
             sum += sumArr[i];
         });
     }
-    if (remainder > 0) {
-        for (int i = (num_threads * numPerThread) - 1; i < N; ++i) {
-            sum += a[i];
-        }
-    }
+
+
     for (int i = 0; i < num_threads; ++i) {
         threads[i].join();
+    }
+    if (remainder > 0) {
+        for (int i = (num_threads * numPerThread); i < N; ++i) {
+            sum += a[i];
+        }
     }
 
     auto stop = high_resolution_clock::now();
@@ -54,18 +56,52 @@ auto parallel_sum_std(T a[], size_t N, size_t num_threads) {
 }
 
 template<typename T>
+T calculate_sum(T a[], size_t N, size_t num_threads, int id, vector<T> &partialSums) {
+    num_threads %= N;
+//    cout << "run" << endl;
+
+    size_t numPerThread = N / num_threads;
+    int startIdx = id * numPerThread;
+    int endIdx = (id * numPerThread) + numPerThread;
+    for (int j = startIdx; j < endIdx; ++j) {
+        partialSums[id] += a[j];
+    }
+    return partialSums[id];
+}
+
+template<typename T>
 auto parallel_sum_omp1(T a[], size_t N, size_t threads) {
     threads %= N;
     atomic<T> sum;
     sum = 0;
+    vector<T> partialSums(threads, NULL);
     auto start = high_resolution_clock::now();
+    size_t remainder = N % threads;
+    size_t numPerThread = N / threads;
 
-    #pragma omp parallel for num_threads(threads)
+
+#pragma omp parallel
     {
-        for (int i = 0; i < N; ++i) {
-            sum += a[i];
+        int id = omp_get_thread_num();
+
+        int startIdx = id * numPerThread;
+        int endIdx = (id * numPerThread) + numPerThread;
+        int count = 0;
+        for (int j = startIdx; j < endIdx; ++j) {
+            partialSums[id] += a[j];
         }
     }
+#pragma omp single
+        {
+            for (int i = 0; i < threads; ++i) {
+                sum += partialSums[i];
+            }
+            if (remainder > 0) {
+                for (int i = (threads * numPerThread); i < N; ++i) {
+                    sum += a[i];
+                }
+            }
+        }
 
     auto stop = high_resolution_clock::now();
     Result<T> result{};
@@ -124,15 +160,15 @@ void graph(int N, int thread_count) {
 }
 
 int main() {
-    for (int i = 1000; i < 10000; i += 1000) {
+/*    for (int i = 1000; i < 10000; i += 1000) {
         graph(i, i / 1000);
-    }
+    }*/
 /*    for (int i = 1000; i < 6000; i += 1000) {
         for (int j = 1; j <= 16; ++j) {
             graph(i, j);
         }
     }*/
-/*    int arrSize = 1000000;
+    int arrSize = 1000005;
     int a[arrSize];//= {34,445,45,23,56,57,67,68,232,5,7,8,78,89,75,2,45,3,3};
     for (int i = 0; i < arrSize; ++i) {
         a[i] = rand() % 100;
@@ -149,6 +185,6 @@ int main() {
     cout << resultThreadedOmpRed.data << endl;
     cout << resultThreadedOmpRed.duration << endl;
     cout << resultNotThreaded.data << endl;
-    cout << resultNotThreaded.duration << endl;*/
+    cout << resultNotThreaded.duration << endl;
     return 0;
 }
